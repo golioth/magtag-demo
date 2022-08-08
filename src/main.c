7f51cdfac9ca6ac548e48084b6ab34a34681734a
Mike Szczys <szczys@hotmail.com>
2022-08-07 20:45:17 -0500

    Create barebones ePaper test

--- src/main.c
+++ src/main.c
@@ -7,66 +7,19 @@
 /* Logging */
 #include <stdlib.h>
 #include <logging/log.h>
-LOG_MODULE_REGISTER(golioth_magtag, LOG_LEVEL_DBG);
+LOG_MODULE_REGISTER(magtag_newepaper, LOG_LEVEL_DBG);
 
 /* MagTag specific hardware includes */
 #include "epaper/EPD_2in9d.h"
 #include "epaper/ImageData.h"
-#include "ws2812/ws2812_control.h"
-
-/* Golioth platform includes */
-#include <net/coap.h>
-#include <net/golioth/system_client.h>
-#include <samples/common/wifi.h>
-
-static struct golioth_client *client = GOLIOTH_SYSTEM_CLIENT_GET();
-
-/*
- * In the `main` function, this function is registed to be
- * called when the device receives a packet from the Golioth server.
- */
-static void golioth_on_message(struct golioth_client *client,
-			       struct coap_packet *rx)
-{
-	uint16_t payload_len;
-	const uint8_t *payload;
-	uint8_t type;
-
-	type = coap_header_get_type(rx);
-	payload = coap_packet_get_payload(rx, &payload_len);
-
-	if (!IS_ENABLED(CONFIG_LOG_BACKEND_GOLIOTH) && payload) {
-		LOG_HEXDUMP_DBG(payload, payload_len, "Payload");
-	}
-}
 
 void main(void)
 {
 	LOG_DBG("Start MagTag Hello demo");
 
-	/* WiFi */
-	if (IS_ENABLED(CONFIG_GOLIOTH_SAMPLE_WIFI)) {
-		LOG_INF("Connecting to WiFi");
-		wifi_connect();
-	}
-
-	client->on_message = golioth_on_message;
-	golioth_system_client_start();
-
-
-	/* Initialize MagTag hardware */
-	ws2812_init();
 	/* show two blue pixels to show until we connect to Golioth */
-	leds_immediate(BLACK, BLUE, BLUE, BLACK);
 	epaper_init();
-
-	/* wait until we've connected to golioth */
-	while (golioth_ping(client) != 0)
-	{
-		k_msleep(1000);
-	}
 	/* turn LEDs green to indicate connection */
-	leds_immediate(GREEN, GREEN, GREEN, GREEN);
 	epaper_autowrite("Connected to Golioth!", 21);
 
 
@@ -75,18 +28,11 @@ void main(void)
 	while (true) {
 		/* Send hello message to the Golioth Cloud */
 		LOG_INF("Sending hello! %d", counter);
-		err = golioth_send_hello(client);
-		if (err) {
-			LOG_WRN("Failed to send hello!");
-		}
-		else
-		{
-			/* Write messages on epaper for user feedback */
-			uint8_t sbuf[24];
-			snprintk(sbuf, sizeof(sbuf) - 1, "Sending hello! %d", counter);
-			epaper_autowrite(sbuf, strlen(sbuf));
-		}
+		/* Write messages on epaper for user feedback */
+		uint8_t sbuf[24];
+		snprintk(sbuf, sizeof(sbuf) - 1, "Sending hello! %d", counter);
+		epaper_autowrite(sbuf, strlen(sbuf));
 		++counter;
-		k_sleep(K_SECONDS(5));
+// 		k_sleep(K_SECONDS(5));
 	}
 }
